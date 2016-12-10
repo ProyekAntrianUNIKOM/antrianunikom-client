8f2adad08d4ba026f1e94f084e0d42420a04c0a1
tiorzfk <tiorezafk@yahoo.com>
2016-12-10 14:51:18 -0800

    fix antrian

--- pmb/admin/angularjs/app.js
+++ pmb/admin/angularjs/app.js
@@ -4,11 +4,34 @@
 var app = angular.module('adminApp', ['ngRoute','ngCookies','chart.js','ng-file-input','summernote','ngAnimate']).controller("chartcont", function ($scope,$http) {
   var host = "http://"+window.location.host;
   var base_url  = host+"/apiantrian/public/api/v1/";
-  var operator = [];
-  var jumlah = [];
+
+  //History Loket
+  $scope.tahunvLoketD = '-';
+  $scope.bulanvLoketD = '-';
+  var no_loket = [];
+  var jumlahLoketD = [];
+  $http.post(base_url+'pmb/history/loket')
+    .success(function (data, status, headers, config) {
+      for (var i=0; i<data.result.length; i++) {
+        no_loket.push('Loket - '+data.result[i].no_loket);
+      }
+      for (var i=0; i<data.result.length; i++) {
+        jumlahLoketD.push(data.result[i].jumlah);
+      }
+    }).error(function (data, status, header, config) {
+      console.log(data);
+  });
+  $scope.labelsLoketD = no_loket;
+  $scope.colorsLoketD = [{backgroundColor:[ '#f7464a', '#00ADF9','#97bbcd', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360']}];
+
+  $scope.dataLoketD = [jumlahLoketD];
+
+  //History Operator
   $scope.tahunv = '-';
   $scope.bulanv = '-';
-  $http.post(base_url+'historyall')
+  var operator = [];
+  var jumlah = [];
+  $http.post(base_url+'pmb/history/operator')
     .success(function (data, status, headers, config) {
       for (var i=0; i<data.result.length; i++) {
         operator.push(data.result[i].nama_operator);
@@ -20,6 +43,7 @@ var app = angular.module('adminApp', ['ngRoute','ngCookies','chart.js','ng-file-
       console.log(data);
   });
   $scope.labels = operator;
+  $scope.colors = [{backgroundColor:[ '#f7464a', '#00ADF9','#97bbcd', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360']}];
   $scope.series = ['Antrian Terlayani'];
 
   $scope.data = [jumlah];
@@ -39,7 +63,7 @@ var app = angular.module('adminApp', ['ngRoute','ngCookies','chart.js','ng-file-
     }
     var operator = [];
     var jumlah = [];
-    $http.post(base_url+'historyall', data, config)
+    $http.post(base_url+'pmb/history/operator', data, config)
       .success(function (data, status, headers, config) {
         for (var i=0; i<data.result.length; i++) {
           operator.push(data.result[i].nama_operator);
@@ -47,14 +71,21 @@ var app = angular.module('adminApp', ['ngRoute','ngCookies','chart.js','ng-file-
         for (var i=0; i<data.result.length; i++) {
           jumlah.push(data.result[i].jumlah);
         }
+        console.log(data.result.length);
+        if(data.result.length==0){
+          $scope.msg = 'Data not found.';
+        }else{
+          $scope.msg = '';
+        }
       }).error(function (data, status, header, config) {
         console.log(data);
     });
     $scope.labels = operator;
     $scope.series = ['Antrian Terlayani'];
-
     $scope.data = [jumlah];
   }
+
+
 
 }).controller("nav", function ($scope,sessionService,$http,$location) {
   $scope.logout = function() {
@@ -65,6 +96,10 @@ var app = angular.module('adminApp', ['ngRoute','ngCookies','chart.js','ng-file-
   $scope.username = sessionService.get('username');
 }).controller("head",function($scope,$http){
   $http.get(base_url+'history/loket').success(function(data){
+    // var icon = [''];
+    // for (var i = 0; i < data.result.length; i++) {
+    //   data.result[i].icon =
+    // }
     $scope.loketdata = data.result;
   }).error(function(data){
     console.error(data);
@@ -77,36 +112,10 @@ app.value('host',host);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: './views/home.html',controller:'homeController'});
   $routeProvider.when('/login', {templateUrl: './views/login.html',controller:'loginController'});
-  //operator
-  $routeProvider.when('/operator', {templateUrl: './views/operator/operator.html',controller:'operatorController'});
-  $routeProvider.when('/operator/detail/:id', {templateUrl: './views/operator/historyoperator.html',controller:'operatorController'});
-  //berita
-  $routeProvider.when('/berita', {templateUrl: './views/berita/berita.html',controller:'beritaController'});
-  $routeProvider.when('/berita/add', {templateUrl: './views/berita/formberita.html',controller:'beritaController'});
-  $routeProvider.when('/berita/edit/:id_editBerita', {templateUrl: './views/berita/editberita.html',controller:'beritaController'});
-  $routeProvider.when('/berita/detail/:id_detailBerita', {templateUrl: './views/berita/detailberita.html',controller:'beritaController'});
-  $routeProvider.when('/berita/:idhapus', {controller:'beritaController'});
-  //video
-  $routeProvider.when('/video', {templateUrl: './views/video/video.html',controller:'videoController'});
-  $routeProvider.when('/video/add', {templateUrl: './views/video/formvideo.html',controller:'videoController'});
-  $routeProvider.when('/video/edit/:id_editVideo', {templateUrl: './views/video/editvideo.html',controller:'videoController'});
-  $routeProvider.when('/video/detail/:id_detailVideo', {templateUrl: './views/video/detailvideo.html',controller:'videoController'});
-  $routeProvider.when('/video/:idhapus', {controller:'videoController'});
-  //mahasiswa
-  $routeProvider.when('/mahasiswa', {templateUrl: './views/mahasiswa/mahasiswa.html',controller:'mahasiswaController'});
-  $routeProvider.when('/mahasiswa/edit/:id_editMahasiswa', {templateUrl: './views/mahasiswa/editmahasiswa.html',controller:'mahasiswaController'});
-  $routeProvider.when('/mahasiswa/daftar', {templateUrl: './views/mahasiswa/formmahasiswa.html',controller:'mahasiswaController'});
-  //banner
-  $routeProvider.when('/banner', {templateUrl: './views/banner/banner.html',controller:'bannerController'});
-  $routeProvider.when('/banner/add', {templateUrl: './views/banner/formbanner.html',controller:'bannerController'});
-  $routeProvider.when('/banner/edit/:id_editBanner', {templateUrl: './views/banner/editbanner.html',controller:'bannerController'});
 }]);
 
 app.run(function($rootScope,$location,loginService){
-  var routespermission=['/','/operator','/operator/detail/:id','/berita/edit/:id',
-                        '/berita','/berita/add','/berita/detail/:id_detailBerita','/mahasiswa','/mahasiswa/daftar',
-                        '/mahasiswa/edit/:id_editMahasiswa','/banner','/banner/add','/banner/edit/:id_banner','/video',
-                        '/video/add','/video/edit/:id_editVideo','/video/detail/:id_detailVideo','/video/:idhapus'];
+  var routespermission=['/'];
   $rootScope.$on('$routeChangeStart',function(){
     if(routespermission.indexOf($location.path()) !=-1 && !loginService.isLogged()){
       $location.path('/login');
